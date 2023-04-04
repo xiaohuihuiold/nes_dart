2b86f94c4d3b13f985c138e372142a99943b2ab8
xiaohuihuiold <xiaohuihuiold@gmail.com>
2023-04-04 17:36:27 +0800

    feat(cpu): 修改状态寄存器读写与部分寻址模式的实现

--- lib/src/nes_cpu.dart
+++ lib/src/nes_cpu.dart
@@ -1,5 +1,6 @@
 import 'logger.dart';
 import 'nes_emulator.dart';
+import 'nes_cpu_codes.dart';
 
 /// CPU中断
 ///
@@ -35,41 +36,55 @@ class NESCpuRegisters {
   int y = 0;
 
   /// 8位状态寄存器
-  final status = NESCpuStatusRegisters();
+  int status = 0;
 
   /// 16位指令计数器
   int pc = 0;
 
   /// 8位栈指针
   int sp = 0;
+
+  /// 获取状态寄存器flag
+  int getStatus(NESCpuStatusRegister flag) {
+    return (status >> flag.index) & 0x01;
+  }
+
+  /// 设置状态寄存器flag
+  void setStatus(NESCpuStatusRegister flag, int value) {
+    status &= ~flag.bit;
+    status |= (value & 0x01) << flag.index;
+  }
 }
 
 /// 状态寄存器
-class NESCpuStatusRegisters {
+enum NESCpuStatusRegister {
   /// 进位标记,指令结果是否进位
-  int c = 0;
+  c(1 << 0),
 
   /// 零标记,指令结果是否为0
-  int z = 0;
+  z(1 << 1),
 
   /// 禁止中断标记,除NMI中断
-  /// 1为禁止
-  int i = 0;
+  i(1 << 2),
 
   /// 十进制模式标记,NES无作用
-  int d = 0;
+  d(1 << 3),
 
   /// BRK中断时被设置
-  int b = 0;
+  b(1 << 4),
 
   /// 未使用
-  int r = 0;
+  r(1 << 5),
 
   /// 溢出标记,指令结果溢出
-  int v = 0;
+  v(1 << 6),
 
   /// 符号标记
-  int s = 0;
+  s(1 << 7);
+
+  final int bit;
+
+  const NESCpuStatusRegister(this.bit);
 }
 
 /// CPU
@@ -80,7 +95,29 @@ class NESCpu {
   /// 寄存器
   final registers = NESCpuRegisters();
 
+  /// 寻址模式
+  late final _addressingMapping = <NESAddressing, int Function()>{
+    NESAddressing.accumulator: _addressingAccumulator,
+    NESAddressing.implied: _addressingImplied,
+    NESAddressing.immediate: _addressingImmediate,
+    NESAddressing.absolute: _addressingAbsolute,
+    NESAddressing.zeroPage: _addressingZeroPage,
+    NESAddressing.absoluteX: _addressingAbsoluteX,
+    NESAddressing.absoluteY: _addressingAbsoluteY,
+    NESAddressing.zeroPageX: _addressingZeroPageX,
+    NESAddressing.zeroPageY: _addressingZeroPageY,
+    NESAddressing.indirect: _addressingIndirect,
+    NESAddressing.indirectX: _addressingIndirectX,
+    NESAddressing.indirectY: _addressingIndirectY,
+    NESAddressing.relative: _addressingRelative,
+  };
+
   NESCpu(this.emulator);
+
+  /// 寻址
+  int getAddress(NESAddressing mode) {
+    return _addressingMapping[mode]?.call() ?? 0;
+  }
 
   /// 重置
   void reset() {
@@ -100,5 +137,72 @@ class NESCpu {
     logger.i(
         'IRQ/BRK: \$$irqAddress, NMI: \$$nmiAddress, RESET: \$$resetAddress');
     logger.v('CPU已重置');
+  }
+
+  /// 无需处理
+  int _addressingAccumulator() {
+    return 0;
+  }
+
+  /// 无需处理
+  int _addressingImplied() {
+    return 0;
+  }
+
+  /// 地址是当前PC位置
+  int _addressingImmediate() {
+    final address = registers.pc;
+    registers.pc++;
+    return address;
+  }
+
+  int _addressingAbsolute() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingZeroPage() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingAbsoluteX() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingAbsoluteY() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingZeroPageX() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingZeroPageY() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingIndirect() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingIndirectX() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingIndirectY() {
+    // TODO: 实现
+    throw Exception('未实现');
+  }
+
+  int _addressingRelative() {
+    // TODO: 实现
+    throw Exception('未实现');
   }
 }
