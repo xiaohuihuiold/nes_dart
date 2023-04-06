04020ec5fa5dd8daed281643a830413e48cb1e13
xiaohuihuiold <xiaohuihuiold@gmail.com>
2023-04-06 15:14:53 +0800

    test: 优化test

--- lib/nes_dart.dart
+++ lib/nes_dart.dart
@@ -4,4 +4,6 @@ export 'src/nes_emulator.dart';
 export 'src/nes_rom_loader.dart';
 export 'src/nes_rom.dart';
 export 'src/nes_cpu_codes.dart';
+export 'src/nes_mapper.dart';
+export 'src/nes_memory.dart';
 export 'src/constants.dart';
--- test/nes_dart_test.dart
+++ test/nes_dart_test.dart
@@ -3,35 +3,56 @@ import 'package:nes_dart/nes_dart.dart';
 
 void main() {
   test('run nestest', () async {
+    // 查找重复的指令码
     final codeMap = <int, int>{};
     for (final code in NESCpuCodes.nesCpuCodes) {
       codeMap[code.opCode] = (codeMap[code.opCode] ?? 0) + 1;
     }
     codeMap.removeWhere((key, value) => value == 1);
-    print(codeMap.keys.map((e) => e.toRadixString(16)));
-    expect(
-        NESCpuCodes.nesCpuCodeMapping.length, NESCpuCodes.nesCpuCodes.length);
+    expect(codeMap.keys.map((e) => e.toRadixString(16)).toList(), [],
+        reason: '重复定义的指令码');
+
+    // 加载rom
     final rom = await NESRomLoader.loadFromPath('roms/nestest.nes');
     print(rom);
     final emulator = NESEmulator(rom: rom);
     emulator.run();
+
+    // 输出指令
     final prg = emulator.rom.prgRom;
     bool error = false;
+    const addressPRGRom = NESMapper000.addressPRGRom;
     for (int i = 0; i < prg.length;) {
-      final byte = prg[i];
+      final address = addressPRGRom + i;
+      final byte = emulator.mapper.read(address);
       final op = NESCpuCodes.getOP(byte);
+
       if (op.op == NESOp.error) {
         error = true;
         print('错误: ${byte.toRadixString(16)}');
         break;
       }
-      final stringBuffer = StringBuffer(
-          '${i.toRadixString(16).toUpperCase().padLeft(4, '0')}: ${op.op.name}');
-      for (int d = 1; d < op.size; d++) {
-        final byte = prg[i + d].toRadixString(16).toUpperCase();
-        stringBuffer.write(' ');
-        stringBuffer.write(byte);
+
+      final stringBuffer = StringBuffer();
+      final addressStr =
+          address.toRadixString(16).toUpperCase().padLeft(4, '0');
+      stringBuffer.write(addressStr);
+      stringBuffer.write(': ');
+      stringBuffer.write(op.op.name);
+      String value = emulator.mapper
+          .read(address + 1)
+          .toRadixString(16)
+          .toUpperCase()
+          .padLeft(2, '0');
+      if (op.size == 3) {
+        value = emulator.mapper
+            .read16(address + 1)
+            .toRadixString(16)
+            .toUpperCase()
+            .padLeft(2, '0');
       }
+      stringBuffer.write(' ');
+      stringBuffer.write(value);
       print(stringBuffer.toString());
       i += op.size;
     }
